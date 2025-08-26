f8b28de279bdee970f091a8300df3217fce14faa
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-26 20:30:15 -0300

    Atualizando o projeto

--- api-marcacao-consultas-main/src/main/java/com/fiap/eca/api_marcacao_consultas/repository/UsuarioRepository.java
+++ api-marcacao-consultas-main/src/main/java/com/fiap/eca/api_marcacao_consultas/repository/UsuarioRepository.java
@@ -1,6 +1,6 @@
-package com.fiap.ecr.api_marcacao_consultas.repository;
+package com.fiap.eca.api_marcacao_consultas.repository;
 
-import com.fiap.ecr.api_marcacao_consultas.model.Usuario;
+import com.fiap.eca.api_marcacao_consultas.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +10,8 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+
     List<Usuario> findByTipo(String tipo);
+
+    List<Usuario> findByTipoAndEspecialidade(String tipo, String especialidade);
 }
